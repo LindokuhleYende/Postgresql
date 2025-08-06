c62069884bfb82f56e699e7aa350e89fdf168a89
Lindokuhle <yendelindokuhle2@gmail.com>
2025-08-06 09:51:25 +0200

    Create chapter13.sql

---
+++ chapter13.sql
@@ -0,0 +1,155 @@
+CREATE TABLE crime_reports (
+    crime_id bigserial PRIMARY KEY,
+    date_1 timestamp with time zone,
+    date_2 timestamp with time zone,
+    street varchar(250),
+    city varchar(100),
+    crime_type varchar(100),
+    description text,
+    case_number varchar(50),
+    original_text text NOT NULL
+);
+
+COPY crime_reports (original_text)
+FROM 'C:\YourDirectory\crime_reports.csv'
+WITH (FORMAT CSV, HEADER OFF, QUOTE '"');
+
+SELECT original_text FROM crime_reports;
+
+-- Listing 13-3: Using regexp_match() to find the first date
+SELECT crime_id,
+       regexp_match(original_text, '\d{1,2}\/\d{1,2}\/\d{2}')
+FROM crime_reports;
+
+-- Listing 13-4: Using the regexp_matches() function with the 'g' flag
+SELECT crime_id,
+       regexp_matches(original_text, '\d{1,2}\/\d{1,2}\/\d{2}', 'g')
+FROM crime_reports;
+
+-- Listing 13-5: Using regexp_match() to find the second date
+-- Note that the result includes an unwanted hyphen
+SELECT crime_id,
+       regexp_match(original_text, '-\d{1,2}\/\d{1,2}\/\d{1,2}')
+FROM crime_reports;
+
+-- Listing 13-6: Using a capture group to return only the date
+-- Eliminates the hyphen
+SELECT crime_id,
+       regexp_match(original_text, '-(\d{1,2}\/\d{1,2}\/\d{1,2})')
+FROM crime_reports;
+
+-- Listing 13-7: Matching case number, date, crime type, and city
+
+SELECT
+    regexp_match(original_text, '(?:C0|SO)[0-9]+') AS case_number,
+    regexp_match(original_text, '\d{1,2}\/\d{1,2}\/\d{2}') AS date_1,
+    regexp_match(original_text, '\n(?:\w+ \w+|\w+)\n(.*):') AS crime_type,
+    regexp_match(original_text, '(?:Sq.|Plz.|Dr.|Ter.|Rd.)\n(\w+ \w+|\w+)\n')
+        AS city
+FROM crime_reports;
+
+-- Bonus: Get all parsed elements at once
+
+SELECT crime_id,
+       regexp_match(original_text, '\d{1,2}\/\d{1,2}\/\d{2}') AS date_1,
+       CASE WHEN EXISTS (SELECT regexp_matches(original_text, '-(\d{1,2}\/\d{1,2}\/\d{1,2})'))
+            THEN regexp_match(original_text, '-(\d{1,2}\/\d{1,2}\/\d{1,2})')
+            ELSE NULL
+            END AS date_2,
+       regexp_match(original_text, '\/\d{2}\n(\d{4})') AS hour_1,
+       CASE WHEN EXISTS (SELECT regexp_matches(original_text, '\/\d{2}\n\d{4}-(\d{4})'))
+            THEN regexp_match(original_text, '\/\d{2}\n\d{4}-(\d{4})')
+            ELSE NULL
+            END AS hour_2,
+       regexp_match(original_text, 'hrs.\n(\d+ .+(?:Sq.|Plz.|Dr.|Ter.|Rd.))') AS street,
+       regexp_match(original_text, '(?:Sq.|Plz.|Dr.|Ter.|Rd.)\n(\w+ \w+|\w+)\n') AS city,
+       regexp_match(original_text, '\n(?:\w+ \w+|\w+)\n(.*):') AS crime_type,
+       regexp_match(original_text, ':\s(.+)(?:C0|SO)') AS description,
+       regexp_match(original_text, '(?:C0|SO)[0-9]+') AS case_number
+FROM crime_reports;
+
+-- Listing 13-8: Retrieving a value from within an array
+
+SELECT
+    crime_id,
+    (regexp_match(original_text, '(?:C0|SO)[0-9]+'))[1]
+        AS case_number
+FROM crime_reports;
+
+-- Listing 13-9: Updating the crime_reports date_1 column
+
+UPDATE crime_reports
+SET date_1 = 
+(
+    (regexp_match(original_text, '\d{1,2}\/\d{1,2}\/\d{2}'))[1]
+        || ' ' ||
+    (regexp_match(original_text, '\/\d{2}\n(\d{4})'))[1] 
+        ||' US/Eastern'
+)::timestamptz;
+
+SELECT crime_id,
+       date_1,
+       original_text
+FROM crime_reports;
+
+-- Listing 13-10: Updating all crime_reports columns
+
+UPDATE crime_reports
+SET date_1 = 
+    (
+      (regexp_match(original_text, '\d{1,2}\/\d{1,2}\/\d{2}'))[1]
+          || ' ' ||
+      (regexp_match(original_text, '\/\d{2}\n(\d{4})'))[1] 
+          ||' US/Eastern'
+    )::timestamptz,
+             
+    date_2 = 
+    CASE 
+    -- if there is no second date but there is a second hour
+        WHEN (SELECT regexp_match(original_text, '-(\d{1,2}\/\d{1,2}\/\d{1,2})') IS NULL)
+                     AND (SELECT regexp_match(original_text, '\/\d{2}\n\d{4}-(\d{4})') IS NOT NULL)
+        THEN 
+          ((regexp_match(original_text, '\d{1,2}\/\d{1,2}\/\d{2}'))[1]
+              || ' ' ||
+          (regexp_match(original_text, '\/\d{2}\n\d{4}-(\d{4})'))[1] 
+              ||' US/Eastern'
+          )::timestamptz 
+
+    -- if there is both a second date and second hour
+        WHEN (SELECT regexp_match(original_text, '-(\d{1,2}\/\d{1,2}\/\d{1,2})') IS NOT NULL)
+              AND (SELECT regexp_match(original_text, '\/\d{2}\n\d{4}-(\d{4})') IS NOT NULL)
+        THEN 
+          ((regexp_match(original_text, '-(\d{1,2}\/\d{1,2}\/\d{1,2})'))[1]
+              || ' ' ||
+          (regexp_match(original_text, '\/\d{2}\n\d{4}-(\d{4})'))[1] 
+              ||' US/Eastern'
+          )::timestamptz 
+    -- if neither of those conditions exist, provide a NULL
+        ELSE NULL 
+    END,
+    street = (regexp_match(original_text, 'hrs.\n(\d+ .+(?:Sq.|Plz.|Dr.|Ter.|Rd.))'))[1],
+    city = (regexp_match(original_text,
+                           '(?:Sq.|Plz.|Dr.|Ter.|Rd.)\n(\w+ \w+|\w+)\n'))[1],
+    crime_type = (regexp_match(original_text, '\n(?:\w+ \w+|\w+)\n(.*):'))[1],
+    description = (regexp_match(original_text, ':\s(.+)(?:C0|SO)'))[1],
+    case_number = (regexp_match(original_text, '(?:C0|SO)[0-9]+'))[1];
+
+-- Listing 13-11: Viewing selected crime data
+
+SELECT date_1,
+       street,
+       city,
+       crime_type
+FROM crime_reports;
+
+-- Listing 13-12: Using regular expressions in a WHERE clause
+
+SELECT geo_name
+FROM us_counties_2010
+WHERE geo_name ~* '(.+lade.+|.+lare.+)'
+ORDER BY geo_name;
+
+SELECT geo_name
+FROM us_counties_2010
+WHERE geo_name ~* '.+ash.+' AND geo_name !~ 'Wash.+'
+ORDER BY geo_name;
